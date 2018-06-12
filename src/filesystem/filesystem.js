bf5b30dae6546cd21fc19eff7f94c5cdfa6142d5
Deepak Thukral <deepak.thukral@fagbokforlaget.no>
2018-06-12 09:22:55 +0200

    fix: single quote strings

--- src/filesystem/filesystem.js
+++ src/filesystem/filesystem.js
@@ -161,7 +161,7 @@ export default class FileSystem {
     return this.exists(path)
       .then((data) => {
         if (data) {
-          return this.storage.getBy("parentId", data.id)
+          return this.storage.getBy('parentId', data.id)
         } else {
           return new Error('path does not exists')
         }
